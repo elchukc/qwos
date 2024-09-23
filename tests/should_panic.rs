e496dcef2781870c6a06103830f6e451f6b73e81
elchc <clareelchuk@gmail.com>
2024-09-23 11:47:35 -0400

    no harness needed for single-test files

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,15 +1,13 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
-
 use core::panic::PanicInfo;
 use qwos::{QemuExitCode, exit_qemu, serial_println, serial_print};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  test_main();
+  should_fail();
+  serial_println!("[test did not panic]");
+  exit_qemu(QemuExitCode::Failed);
   loop {}
 }
 
@@ -20,17 +18,6 @@ fn panic(_info: &PanicInfo) -> ! {
   loop{}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-  serial_println!("Running {} tests", tests.len());
-  for test in tests {
-    test();
-    serial_println!("[test did not panic]");
-    exit_qemu(QemuExitCode::Failed);
-  }
-  exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
   serial_print!("should_panic::should_fail...\t");
   assert_eq!(0,1);
