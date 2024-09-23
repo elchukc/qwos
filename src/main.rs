a2a49379c5803208adefb9c6bd228c75806ee8ec
elchc <clareelchuk@gmail.com>
2024-09-23 11:33:22 -0400

    refactor to integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(qwos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-  println!("{}", info);
-  loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-  serial_println!("[failed]\n");
-  serial_println!("Error: {}\n", info);
-  exit_qemu(QemuExitCode::Failed);
-  loop {}
-}
+use qwos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -35,32 +17,18 @@ pub extern "C" fn _start() -> ! {
   loop {}
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-  Success = 0x10,
-  Failed = 0x11,
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
+  loop {}
 }
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-  use x86_64::instructions::port::Port;
-  unsafe {
-    let mut port = Port::new(0xf4);
-    port.write(exit_code as u32);
-  }
-}
-
-pub trait Testable {
-  fn run (&self) -> ();
-}
-impl<T> Testable for T
-where T: Fn(),
-{
-  fn run (&self) -> () {
-    serial_print!("{}...\t", core::any::type_name::<T>());
-    self();
-    serial_println!("[ok]");
-  }
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
+  loop {}
 }
 
 #[test_case]
@@ -73,13 +41,4 @@ fn test_println_many() {
   for _ in 0..200 {
     println!("test_println_many output");
   }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-  serial_println!("Running {} tests", tests.len());
-  for test in tests {
-    test.run();
-  }
-  exit_qemu(QemuExitCode::Success);
 }
