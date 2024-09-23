5220c51a17e0d7ed29990a6c0931fb928cc3c6b8
elchc <clareelchuk@gmail.com>
2024-09-22 22:53:32 -0400

    test runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -13,11 +16,41 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  use core::fmt::Write;
-  vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+  println!("Hello World{}", "!");
 
-  write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-  panic!("Some panic message");
+  #[cfg(test)]
+  test_main();
 
   loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+  Success = 0x10,
+  Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+  use x86_64::instructions::port::Port;
+  unsafe {
+    let mut port = Port::new(0xf4);
+    port.write(exit_code as u32);
+  }
+}
+
+#[test_case]
+fn trivial_assertion() {
+  print!("trivial assertion... ");
+  assert_eq!(1,1);
+  println!("[ok]");
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+  println!("Running {} tests", tests.len());
+  for test in tests {
+    test();
+  }
+  exit_qemu(QemuExitCode::Success);
 }
