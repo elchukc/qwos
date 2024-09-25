32da7d4e39ed0335a66b4b6a99d268e13885d631
elchc <clareelchuk@gmail.com>
2024-09-25 11:48:59 -0400

    CPU Exception handling

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 pub mod vga_buffer;
 pub mod serial;
+pub mod interrupts;
 
 pub trait Testable {
   fn run (&self) -> ();
@@ -21,6 +23,10 @@ where T: Fn(),
     self();
     serial_println!("[ok]");
   }
+}
+
+pub fn init() {
+  interrupts::init_idt();
 }
 
 pub fn test_runner(tests: &[&dyn Testable]) {
@@ -42,6 +48,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+  init();
   test_main();
   loop {}
 }
